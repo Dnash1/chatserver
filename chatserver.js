7f63e4f25f9b28e461a19b041da631425808069a
David Nash <david.allyxanddar.nash@gmail.com>
2015-07-28 18:01:56 -0400

    chat works with usernames and /yell

--- chatserver.js
+++ chatserver.js
@@ -5,24 +5,41 @@ var port = 2000;
 
 var users = [];
 
-var server = net.createServer();
+var server = net.createServer(function(connection) {
+	var msgAll = function msgAll(message, sender) {
+		users.forEach(function(user) {
+			if (user === sender) {
 
-server.on("connection", function(client) {
-	console.log("New user connected: " + client.remoteAddress);
-	client.write("Welcome user! \n");
-	for (i = 0; i < users.length; i++) {
-			users[i].write(client.remoteAddress + " connected! \n");
-			console.log(client.remoteAddress + " connected! \n");
+			} else {
+				user.write(message);
+			}
+		console.log(message.toString());
+		})
+	}
+
+	connection.name = connection.remoteAddress;
+	users.push(connection);
+
+	connection.write("Welcome " + connection.name + "\n");
+	msgAll(connection.name + " joined \n", connection);
+
+	connection.on("data", function(data) {
+		if (data.toString().indexOf("/yell") != -1) {
+			msgAll(connection.name + ": " + data.toString().replace(/\/yell/g, "").toUpperCase(), connection);
+		} else if (data.toString().indexOf("/name") != -1) {
+			connection.name = data.toString().replace(/\/name/g, "");
+		} else {
+			msgAll(connection.name + ": " + data, connection);
 		}
-	users.push(client);
-	client.on("data", function(data) {
-		for (i = 0; i < users.length; i++) {
-			users[i].write(client.remoteAddress + ": " + data);
-			console.log(client.remoteAddress + ": " + data.toString());
-		}
+
 	})
-})
 
+	connection.on("end", function () {
+		users.splice(users.indexOf(connection), 1);
+		msgAll(connection.name + " left \n");
+
+	})
+});
 
 
 server.listen(port, function() {
