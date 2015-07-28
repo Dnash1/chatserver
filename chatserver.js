208e61519bebde450ec93a6afdcec64de48ea20a
David Nash <david.allyxanddar.nash@gmail.com>
2015-07-28 16:17:27 -0400

    basic chat workin

--- chatserver.js
+++ chatserver.js
@@ -3,9 +3,24 @@ var fs = require("fs");
 
 var port = 2000;
 
-var server = net.createServer(function(connection) {
-	console.log("user connected");
+var users = [];
+
+var server = net.createServer();
+
+server.on("connection", function(client) {
+	console.log("New user connected: " + client.remoteAddress);
+	client.write("Welcome user! \n");
+	for (i = 0; i < users.length; i++) {
+			users[i].write(client.remoteAddress + " connected! \n");
+		}
+	users.push(client);
+	client.on("data", function(data) {
+		for (i = 0; i < users.length; i++) {
+			users[i].write(client.remoteAddress + ": " + data);
+		}
+	})
 })
+
 
 
 server.listen(port, function() {
